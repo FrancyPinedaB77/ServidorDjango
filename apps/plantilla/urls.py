e8805681fd5a4caa6a48980ea73700a045a55f20
FRANCY <francypineda@gmail.com>
2016-10-19 14:42:22 -0500

    Inicio del servidorDjango para Taller 3

---
+++ apps/plantilla/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+	url(r'^taller2$', views.index, name='index'),
+ 	url(r'^$', views.inicio),
+ 	url(r'^graph$', views.mygraph, name='mygraph'),
+ 	url(r'^grafo$', views.grafo, name='mygraph'),
+ 	url(r'^Taller3$', views.taller3, name='taller3'),
+	]
+
