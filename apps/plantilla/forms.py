002ad49ab0e6b132411de523658770d2be4fb2c1
FrancyPinedaB77 <francypineda77@gmail.com>
2016-10-22 23:44:57 -0500

    hace 3 graficas con escala logaritmica pero toca arreglar que traiga el nombre y los seguidores y amigos por usuario y colocar todos los elemenos de la lista desplegable

--- apps/plantilla/forms.py
+++ apps/plantilla/forms.py
@@ -5,5 +5,7 @@ class forms_inicio(forms.Form):
     numtuits = forms.CharField(label='numtuits', max_length=20)
     otro = forms.CharField(label='otro', max_length=20)
 
+class form_usuario(forms.Form):
+    CHOICES = (('JuanManSantos', 'Juan Manuel Santos'),('AlvaroUribeVel', 'Alvaro Uribe Velez'),)
+    usuario = forms.ChoiceField(choices=CHOICES)
 
- 
