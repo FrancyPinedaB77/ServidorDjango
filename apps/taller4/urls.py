b398ae9932377d3f1d78c227710029a4b4ec04df
FrancyPinedaB77 <francypineda77@gmail.com>
2016-11-24 20:24:06 -0500

    Sube dos puntos del mundo 1 quemado Bogota 1 uno España

--- apps/taller4/urls.py
+++ apps/taller4/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 	url(r'^taller4_parte1$', views.taller4_parte1, name='taller4p1'),
     url(r'^taller4_parte2$', views.taller4_parte2, name='taller4p2'),
 	url(r'^taller4_parte3$', views.taller4_parte3, name='taller4p3'),
-
+	url(r'^taller4_parte4$', views.taller4_parte4, name='taller4p4'),
 	]
--- apps/taller4/views.py
+++ apps/taller4/views.py
@@ -2,17 +2,16 @@ from django.shortcuts import render
 import ner
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
+import sys
+import SPARQLWrapper
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
 
 # Create your views here.
 def taller4_parte1(request):
-    b="funciona"
-    st = StanfordNERTagger('/home/xubuntu/Taller4/nueva/classifiers/english.all.3class.distsim.crf.ser.gz','/home/xubuntu/Taller4/nueva/stanford-ner.jar', encoding='utf-8')
-    text = 'While in France, Christine Lagarde discussed short-term stimulus efforts in a recent interview with the Wall Street Journal.'
-    tokenized_text = word_tokenize(text)
-    classified_text = st.tag(tokenized_text)
-    print(classified_text)
-    return render(request, "taller4_parte1.html",{"classified_text":classified_text,"b":b})
-
+    numero2=78
+    return render(request, "taller4_parte1.html",{"numero2":numero2})
 
 def taller4_parte2(request):
     numero2=78
@@ -20,4 +19,39 @@ def taller4_parte2(request):
 
 def taller4_parte3(request):
     numero3=79
-    return render(request, "taller4_parte3.html",{"numero3":numero3})
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setQuery("""
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    SELECT ?lat
+    WHERE { <http://dbpedia.org/resource/Spain> geo:lat ?lat }
+""")
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    for result in results["results"]["bindings"]:
+        a=2
+        #print (result["lat"]["value"]).decode(string)
+    lat=(result["lat"]["value"])    
+    #Trae los valores de longitud 
+    sparql.setQuery("""
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT ?long
+        WHERE { <http://dbpedia.org/resource/Spain> geo:long ?long }
+    """)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    for result in results["results"]["bindings"]:
+        a=2
+        #print (result["long"]["value"])
+    longit=(result["long"]["value"]) 
+    return render(request, "taller4_parte3.html",{"longit":longit,"lat":lat})
+
+
+def taller4_parte4(request):
+    b="funciona"
+    st = StanfordNERTagger('/home/xubuntu/Taller4/nueva/classifiers/english.all.3class.distsim.crf.ser.gz','/home/xubuntu/Taller4/nueva/stanford-ner.jar', encoding='utf-8')
+    text = 'While in France, Christine Lagarde discussed short-term stimulus efforts in a recent interview with the Wall Street Journal.'
+    tokenized_text = word_tokenize(text)
+    classified_text = st.tag(tokenized_text)
+    print(classified_text)
+
+    return render(request, "taller4_parte4.html",{"classified_text":classified_text,"b":b})
